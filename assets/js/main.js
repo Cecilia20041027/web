4d2907179b4b23b68a7c8ddfdc14e74f2c655b3f
Cecilia20041027 <melodychou1027@gmail.com>
2026-05-29 23:49:08 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -138,8 +138,8 @@
 
     isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
       filters.addEventListener('click', function() {
-        isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
-        this.classList.add('filter-active');
+        isotopeItem.querySelector('.isotope-filters .filter-all').classList.remove('filter-all');
+        this.classList.add('filter-all');
         initIsotope.arrange({
           filter: this.getAttribute('data-filter')
         });
